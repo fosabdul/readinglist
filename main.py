4437a3d6620d2ad557a42da73598a5cb9d86afce
MinaYimam <77759463+MinaYimam@users.noreply.github.com>
2022-02-08 10:39:58 -0600

    i have made changed on the modfying book and its not crashing when the user is adding a book not in the database

--- main.py
+++ main.py
@@ -60,10 +60,14 @@ def search_book():
 
 def change_read():
     book_id = ui.get_book_id()
-    book = store.get_book_by_id(book_id)  
-    new_read = ui.get_read_value()     
-    book.read = new_read 
-    book.save()
+    book = store.get_book_by_id(book_id)
+    if book:
+        new_read = ui.get_read_value()
+        book.read = new_read
+        book.save()
+    else:
+        print('There is no book found with that id!')
+
 
 
 def delete_book():
